395df1cac765db094d22d912b78e652637029e7d
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-03-07 13:36:40 -0600

    for loop to add ints in an array

--- problem0.java
+++ problem0.java
@@ -1,1 +1,12 @@
-//write three functions that compute the sum of he numbers in a given list using a for-loop, a while-loop, and recursion
+//write three functions that compute the sum of the numbers in a given list using a for-loop, a while-loop, and recursion
+
+int sum = 0;
+int[] arr1 = new int[4];
+
+
+for(int i=o; i<arr1.length;i++){
+    sum+=i;
+}
+
+sum=0;
+while()
