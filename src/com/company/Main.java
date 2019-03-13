3e1fcf27c42a8293b186c8d649ba754be42a78c6
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-03-13 15:47:29 -0500

    Added recurive loop and deleted while and for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,35 @@ public class Main {
 
     public static void main(String[] args) {
         //create and fill array
-        int[] arr1;
-        arr1 = new int[10];
-        int i = 0;
-        while(i<arr1.length){
-            arr1[i]= 2;
-            i++;
-        }
+        int[] a = { 1, 2, 3, 4, 5 };
+        int sum = sumOfArray(a, a.length-1);
 
-
-        System.out.println(forAdd(arr1));
     }
-    private static int forAdd(int[] arr2){
 
-        int sum = 0;
-        for (int anArr1 : arr2) {
-            sum+=anArr1;
-        }
-        return sum;
+    //recursive sumOfArray
+    public int sumOfArray(int[] a, int n) {
+        if (n == 0)
+            return a[n];
+        else
+            return a[n] + sumOfArray(a, n-1);
     }
+
+    //while loop sum the array
+    public int whileSumArray(int [] a, int n){
+        int sum =0;
+        while(n<a.length){
+            
+        }
+    }
+    
+    // // for loop forAdd 
+    // public int forAdd(int[] arr2){
+
+    //     int sum = 0;
+    //     for (int anArr1 : arr2) {
+    //         sum+=anArr1;
+    //     }
+    //     return sum;
+    // }
 
 }
