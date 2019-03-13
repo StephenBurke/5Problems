ca728451e1c4a4f18a14fdb641463edf42099976
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-03-13 16:13:12 -0500

    added while and for loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,34 +6,42 @@ public class Main {
     public static void main(String[] args) {
         //create and fill array
         int[] a = { 1, 2, 3, 4, 5 };
-        int sum = sumOfArray(a, a.length-1);
+
+        //test methods  
+        int sum0 = recursiveSumOfArray(a, a.length-1);
+        int sum1 = whileSumOfArray(a);
+        int sum2 = forSumOfArray(a);
+
+        System.out.println("The three sums are" + sum0 + sum1 + sum2);
 
     }
 
-    //recursive sumOfArray
-    public int sumOfArray(int[] a, int n) {
+    //recursive recursiveSumOfArray
+    public int recursiveSumOfArray(int[] a, int n) {
         if (n == 0)
             return a[n];
         else
-            return a[n] + sumOfArray(a, n-1);
+            return a[n] + recursiveSumOfArray(a, n-1);
     }
 
     //while loop sum the array
-    public int whileSumArray(int [] a, int n){
+    public int whileSumOfArray(int [] a){
         int sum =0;
+        int n =0;
         while(n<a.length){
-            
+            sum+=a[n];
+            n++;
         }
+        return sum;
     }
     
-    // // for loop forAdd 
-    // public int forAdd(int[] arr2){
-
-    //     int sum = 0;
-    //     for (int anArr1 : arr2) {
-    //         sum+=anArr1;
-    //     }
-    //     return sum;
-    // }
+    // for loop forSumOfArray 
+    public int forSumOfArray(int[] arr2){
+        int sum = 0;
+        for (int anArr1 : arr2) {
+            sum+=anArr1;
+        }
+        return sum;
+    }
 
 }
